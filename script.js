88d074fc9eae50a2c80687b58ce93d1e4a310ec0
Vlad <f0ntastish@gmail.com>
2020-03-04 20:35:59 +0300

    layout for basic calc

--- script.js
+++ script.js
@@ -1,5 +1,121 @@
 let memory = "";
 
+const layout = [
+    [
+        {
+            type: 'func',
+            title: 'AC',
+            handler: '',
+        },
+        {
+            type: 'func',
+            title: '+/-',
+            handler: '',
+        },
+        {
+            type: 'func',
+            title: '%',
+            handler: '',
+        },
+        {
+            type: 'operator',
+            title: '/',
+            handler: '',
+        }
+    ],
+    [
+        {
+            type: 'num',
+            title: '7',
+            handler: '',
+        },
+        {
+            type: 'num',
+            title: '8',
+            handler: '',
+        },
+        {
+            type: 'num',
+            title: '9',
+            handler: '',
+        },
+        {
+            type: 'operator',
+            title: '*',
+            handler: '',
+        }
+
+    ],
+    [
+        {
+            type: 'num',
+            title: '4',
+            handler: '',
+        },
+        {
+            type: 'num',
+            title: '5',
+            handler: '',
+        },
+        {
+            type: 'num',
+            title: '6',
+            handler: '',
+        },
+        {
+            type: 'operator',
+            title: '-',
+            handler: '',
+        }
+    ],
+    [
+        {
+            type: 'num',
+            title: '1',
+            handler: '',
+        },
+        {
+            type: 'num',
+            title: '2',
+            handler: '',
+        },
+        {
+            type: 'num',
+            title: '3',
+            handler: '',
+        },
+        {
+            type: 'operator',
+            title: '+',
+            handler: '',
+        }
+    ],
+    [
+        {
+            type: 'num',
+            title: '0',
+            handler: '',
+        },
+        {
+            type: 'num',
+            title: '.',
+            handler: '',
+        },
+        {
+            type: 'operator',
+            title: '=',
+            handler: '',
+        }
+    ]
+]
+
+
+
+
+
+
+
+
 [...document.getElementsByClassName("buttNum")]
     .forEach(x => {
     x.addEventListener("click", function () {
@@ -7,38 +123,38 @@ let memory = "";
     })
 })
 
-equally.addEventListener("click",function () {
-    display.value=eval(display.value)
+equally.addEventListener("click", function () {
+    display.value = eval(display.value)
 });
 
-AC.addEventListener("click",function(){
+AC.addEventListener("click", function () {
     display.value = "";
 })
 
-plusminus.addEventListener("click",function(){
-    if(display.value === ""){
+plusminus.addEventListener("click", function () {
+    if (display.value === "") {
         display.value = "-"
     }
 })
 
-percent.addEventListener("click",function () {
+percent.addEventListener("click", function () {
     equally.click();
-    display.value = display.value/100;
+    display.value = display.value / 100;
 })
 
-point.addEventListener("click",function () {
-    if(display.value.length == 0){
+point.addEventListener("click", function () {
+    if (display.value.length == 0) {
         display.value = "0";
     }
     display.value = display.value + "."
 })
 
-mc.addEventListener("click",function(){
+mc.addEventListener("click", function () {
     memory = "";
 })
 
-mplus.addEventListener("click",function(){
-    if(memory.length == 0){
+mplus.addEventListener("click", function () {
+    if (memory.length == 0) {
         memory = display.value;
     } else {
         equally.click();
@@ -46,8 +162,8 @@ mplus.addEventListener("click",function(){
     }
 })
 
-mminus.addEventListener("click",function(){
-    if(memory.length == 0){
+mminus.addEventListener("click", function () {
+    if (memory.length == 0) {
         memory = display.value;
     } else {
         equally.click();
@@ -55,58 +171,53 @@ mminus.addEventListener("click",function(){
     }
 })
 
-mr.addEventListener("click",function(){
+mr.addEventListener("click", function () {
     display.value = memory;
 })
 
-powByTwo.addEventListener("click",function(){
+powByTwo.addEventListener("click", function () {
     display.value = eval(display.value * display.value);
 })
 
-powByThree.addEventListener("click",function(){
+powByThree.addEventListener("click", function () {
     display.value = eval(display.value * display.value * display.value);
 })
 
-fac.addEventListener("click", function(){
+fac.addEventListener("click", function () {
     equally.click()
     display.value = factorial(display.value)
 })
 
-rand.addEventListener("click",function(){
-    display.value =+ Math.round(Math.random()*100  )
+rand.addEventListener("click", function () {
+    display.value = + Math.round(Math.random() * 100)
 })
 
-basic.addEventListener("click",function(){
+basic.addEventListener("click", function () {
     document.querySelector("#buttonsBasic").style.display = "flex";
     document.querySelector("#buttonsEngineer").style.display = "none";
     document.querySelector("#buttonsProg").style.display = "none";
     document.querySelector("#calculator").style.width = "400px";
 })
 
-engineer.addEventListener("click",function () {
+engineer.addEventListener("click", function () {
     document.querySelector("#buttonsBasic").style.display = "flex";
-    document.querySelector("#buttonsEngineer").style.display= "flex";
+    document.querySelector("#buttonsEngineer").style.display = "flex";
     document.querySelector("#buttonsProg").style.display = "none";
     document.querySelector("#calculator").style.width = "1000px";
 })
 
-prog.addEventListener("click",function(){
+prog.addEventListener("click", function () {
     document.querySelector("#buttonsBasic").style.display = "none";
     document.querySelector("#buttonsEngineer").style.display = "none";
     document.querySelector("#buttonsProg").style.display = "flex";
     document.querySelector("#calculator").style.width = "700px";
 })
 
-document.addEventListener("DOMContentLoaded", function(){
-    engineer.click();
-})
-
-
 
 function factorial(n) {
     n = eval(n);
-    let fac=1;
-    for (let i = n; i>0; i--)
+    let fac = 1;
+    for (let i = n; i > 0; i--)
         fac *= i;
     return fac;
 }
