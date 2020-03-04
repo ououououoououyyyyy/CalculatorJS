87c1a3e5efc5c74992390b5e143a275b72389d6d
Vlad <f0ntastish@gmail.com>
2020-03-04 18:28:17 +0300

    all on flex

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let basicCalc = document.getElementById("calculator");
+let memory = "";
 
 [...document.getElementsByClassName("buttNum")]
     .forEach(x => {
@@ -21,27 +21,65 @@ plusminus.addEventListener("click",function(){
     }
 })
 
-engineer.addEventListener("click",function () {
-    document.querySelector("#buttonsEngineer").style.display= "flex";
-    document.querySelector("#calculator").style.maxWidth = "1000px"
-    document.querySelector("#buttonsProg").style.display = "none";
-    document.querySelector("#buttonsBasic").style.display = "flex"
+percent.addEventListener("click",function () {
+    equally.click();
+    display.value = display.value/100;
+})
+
+point.addEventListener("click",function () {
+    if(display.value.length == 0){
+        display.value = "0";
+    }
+    display.value = display.value + "."
+})
+
+mc.addEventListener("click",function(){
+    memory = "";
+})
+
+mplus.addEventListener("click",function(){
+    if(memory.length == 0){
+        memory = display.value;
+    } else {
+        equally.click();
+        memory = (+display.value + +memory).toString();
+    }
+})
+
+mminus.addEventListener("click",function(){
+    if(memory.length == 0){
+        memory = display.value;
+    } else {
+        equally.click();
+        memory = (+display.value - +memory).toString();
+    }
+})
+
+mr.addEventListener("click",function(){
+    display.value = memory;
 })
 
 basic.addEventListener("click",function(){
+    document.querySelector("#buttonsBasic").style.display = "flex";
     document.querySelector("#buttonsEngineer").style.display = "none";
-    document.querySelector("#buttonsBasic").style.display = "flex"
-    document.querySelector("#calculator").style.maxWidth = "400px"
+    document.querySelector("#buttonsProg").style.display = "none";
+    document.querySelector("#calculator").style.width = "400px";
+})
+
+engineer.addEventListener("click",function () {
+    document.querySelector("#buttonsBasic").style.display = "flex";
+    document.querySelector("#buttonsEngineer").style.display= "flex";
+    document.querySelector("#buttonsProg").style.display = "none";
+    document.querySelector("#calculator").style.width = "1000px";
 })
 
 prog.addEventListener("click",function(){
-    document.querySelector("#display").style.width = "698px";
-    document.querySelector(".buttonsProg").style.display = "flex";
-    document.querySelector(".buttonsBasic").style.display = "none";
-    document.querySelector(".buttonsEngineer").style.display = "none";
+    document.querySelector("#buttonsBasic").style.display = "none";
+    document.querySelector("#buttonsEngineer").style.display = "none";
+    document.querySelector("#buttonsProg").style.display = "flex";
+    document.querySelector("#calculator").style.width = "700px";
 })
 
-
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function(){
     engineer.click();
 })
